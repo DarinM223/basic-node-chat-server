46ccbbe5c1b7b8610a2196e64e6c76236fff19a9
Darin Minamoto <darinm223@gmail.com>
2014-08-07 18:32:31 -0700

    Abstracted out socket factory

--- public/js/controller.js
+++ public/js/controller.js
@@ -1,39 +1,7 @@
 'use strict';
 
-var chatApp = angular.module('chatApp', [
-    'ngRoute',
-]);
+var chatApp = angular.module('chatApp');
 var connectionURL = 'http://localhost:3700';
-
-// creates a socket that wraps the functions of socket.io
-chatApp.factory('socket', function ($rootScope) {
-  var socket = io.connect(connectionURL);
-
-  return {
-    on: function (eventName, callback) {
-      socket.on(eventName, function () {
-        var args = arguments;
-        $rootScope.$apply(function () {
-          callback.apply(socket, args);
-        });
-      });
-    },
-    emit: function (eventName, data, callback) {
-      socket.emit(eventName, data, function () {
-        var args = arguments;
-        $rootScope.$apply(function () {
-          if (callback) {
-            callback.apply(socket, args);
-          }
-        });
-      });
-    }, 
-    restart: function() {
-      socket.io.disconnect();
-      socket.io.reconnect();
-    }
-  };
-});
 
 // creates a ng-enter directive that fires when the
 // enter key is pressed
@@ -51,272 +19,282 @@ chatApp.directive('ngEnter', function() {
   };
 });
 
+chatApp.factory('messageListFactory', function($rootScope) {
+  var messageList = [];
+  return messageList;
+});
+
+chatApp.factory('userListFactory', function($rootScope) {
+  var userList = []; 
+  return userList;
+});
+
 // main controller for the application, controls the
 // message and user lists and the current logged in user
-chatApp.controller('listControl', ['$scope', 'socket', function ($scope, socket) {
-    $scope.messageList = [];
-    $scope.userList = [];
-    $scope.hiddenUsers = {};
-    $scope.selectedIndex = -1;
-    $scope.my_username = null;
-    $scope.loginWarning = '';
-    $scope.signupWarning = '';
-    $scope.dropdownText = 'Message text';
+chatApp.controller('listControl', function ($scope, socket) {
+  $scope.messageList = [];
+  $scope.userList = [];
+  $scope.hiddenUsers = {};
+  $scope.selectedIndex = -1;
+  $scope.my_username = null;
+  $scope.loginWarning = '';
+  $scope.signupWarning = '';
+  $scope.dropdownText = 'Message text';
 
-    // grab current list of users at the start
-    socket.emit('list');
+  // grab current list of users at the start
+  socket.emit('list');
 
-    socket.on('message', function(data) {
-      console.log(data);
-      $scope.messageList.unshift(data);
-    });
+  socket.on('message', function(data) {
+    console.log(data);
+    $scope.messageList.unshift(data);
+  });
 
-    socket.on('userlogin', function(data) {
-      var message = {
-        message: data.username + ' has connected to the server'
-      };
-      $scope.messageList.unshift(message);
-      var notInUserList = true;
+  socket.on('userlogin', function(data) {
+    var message = {
+      message: data.username + ' has connected to the server'
+    };
+    $scope.messageList.unshift(message);
+    var notInUserList = true;
 
-      for (var i = 0; i < $scope.userList.length; i++) {
-        if ($scope.userList[i] === data.username) {
-          notInUserList = false;
-        }
+    for (var i = 0; i < $scope.userList.length; i++) {
+      if ($scope.userList[i] === data.username) {
+        notInUserList = false;
       }
-      // add user to list if not already there
-      if (notInUserList && data.username !== $scope.my_username) {
-        $scope.userList.push(data.username);
-      }
-    });
+    }
+    // add user to list if not already there
+    if (notInUserList && data.username !== $scope.my_username) {
+      $scope.userList.push(data.username);
+    }
+  });
 
-    socket.on('userlogout', function(data) {
-      var message = {
-        message: data.username + ' has disconnected to the server'
-      };
-      $scope.messageList.unshift(message);
+  socket.on('userlogout', function(data) {
+    var message = {
+      message: data.username + ' has disconnected to the server'
+    };
+    $scope.messageList.unshift(message);
 
-      // remove user from list
-      for (var i = 0; i < $scope.userList.length; i++) {
-        if ($scope.userList[i] === data.username) {
-          $scope.userList.splice(i, 1);
-        }
+    // remove user from list
+    for (var i = 0; i < $scope.userList.length; i++) {
+      if ($scope.userList[i] === data.username) {
+        $scope.userList.splice(i, 1);
       }
-      $scope.hiddenUsers[data.username] = null;
-    });
+    }
+    $scope.hiddenUsers[data.username] = null;
+  });
 
-    socket.on('list', function(data) {
-      // clear user list
-      while ($scope.userList.length > 0) {
-        $scope.userList.pop();
-      }
-      // push usernames into list
-      for (var index = 0; index < data.list.length; index++) {
-        $scope.userList.push(data.list[index]);
-      }
-    });
+  socket.on('list', function(data) {
+    // clear user list
+    while ($scope.userList.length > 0) {
+      $scope.userList.pop();
+    }
+    // push usernames into list
+    for (var index = 0; index < data.list.length; index++) {
+      $scope.userList.push(data.list[index]);
+    }
+  });
 
-    socket.on('login-response', function(data) {
-      // if login successful, change current username, otherwise push error message
-      if (data.username) {
-        $scope.my_username = data.username;
-        $('#modalLogin').modal('hide');
-      } else {
-        //$scope.messageList.unshift(data);
-        $scope.loginWarning = data.error;
-      }
-    });
+  socket.on('login-response', function(data) {
+    // if login successful, change current username, otherwise push error message
+    if (data.username) {
+      $scope.my_username = data.username;
+      $('#modalLogin').modal('hide');
+    } else {
+      //$scope.messageList.unshift(data);
+      $scope.loginWarning = data.error;
+    }
+  });
 
-    socket.on('signup-response', function(data) {
-      if (!data.error) {
-        loginUser(data.username, data.password);
-        $('#modalSignup').modal('hide');
-      } else {
-        $scope.signupWarning = data.error;
-      }
-    });
+  socket.on('signup-response', function(data) {
+    if (!data.error) {
+      loginUser(data.username, data.password);
+      $('#modalSignup').modal('hide');
+    } else {
+      $scope.signupWarning = data.error;
+    }
+  });
 
-    $scope.matchMessage = function(query) {
-      return function(message) {
-        if ($scope.dropdownText === 'Message text') {
-          if (message.message) {
-            return message.message.match(query);
-          } else {
-            return message.error.match(query);
-          }
+  $scope.matchMessage = function(query) {
+    return function(message) {
+      if ($scope.dropdownText === 'Message text') {
+        if (message.message) {
+          return message.message.match(query);
         } else {
-          if (message.username) {
-            return message.username.match(query);
-          } else {
-            return 'Server'.match(query);
-          }
+          return message.error.match(query);
         }
-      };
-    };
-
-    /**
-     * @param {string} chatMessage
-     * @param {string} username
-     * @param {string} receiver
-     * @param {function} callback
-     */
-    function sendMessage(chatMessage, username, receiver, callback) {
-      var newMessage = {
-        'username': username,
-        'message': chatMessage,
-      };
-      if (receiver) {
-        newMessage.receiver = receiver;
+      } else {
+        if (message.username) {
+          return message.username.match(query);
+        } else {
+          return 'Server'.match(query);
+        }
       }
-      socket.emit('send', newMessage, callback);
+    };
+  };
+
+  /**
+   * @param {string} chatMessage
+   * @param {string} username
+   * @param {string} receiver
+   * @param {function} callback
+   */
+  function sendMessage(chatMessage, username, receiver, callback) {
+    var newMessage = {
+      'username': username,
+      'message': chatMessage,
+    };
+    if (receiver) {
+      newMessage.receiver = receiver;
+    }
+    socket.emit('send', newMessage, callback);
+  }
+
+  /**
+   * @param {string} username
+   * @param {string} password
+   * @param {function} callback
+   */
+  function loginUser(username, password, callback) {
+    socket.emit('login', {'username': username, 'password': password}, callback);
+  }
+
+  /**
+   * @param {string} username
+   * @param {string} password
+   * @param {function} callback
+   */
+  function signupUser(username, password, callback) {
+    socket.emit('signup', { 'username': username, 'password': password }, callback);
+  }
+
+  /**
+   * @param {string} chatMessage
+   */
+  $scope.onSend = function(chatMessage) {
+    if ($scope.selectedIndex !== -1) {
+      sendMessage(chatMessage, $scope.my_username, $scope.userList[$scope.selectedIndex]);
+    } else {
+      sendMessage(chatMessage, $scope.my_username, null);
     }
 
-    /**
-     * @param {string} username
-     * @param {string} password
-     * @param {function} callback
-     */
-    function loginUser(username, password, callback) {
-      socket.emit('login', {'username': username, 'password': password}, callback);
+    $scope.clearFields();
+  };
+
+  /**
+   * @param {string} username
+   * @param {string} password
+   */
+  $scope.onLogin = function(username, password) {
+    if (!username || username === '') {
+      $scope.loginWarning = 'Username is empty';
+    } else if (!password || password === '') {
+      $scope.loginWarning = 'Password is empty';
+    } else {
+      loginUser(username, password);
     }
+    $scope.clearFields();
+  };
 
-    /**
-     * @param {string} username
-     * @param {string} password
-     * @param {function} callback
-     */
-    function signupUser(username, password, callback) {
-      socket.emit('signup', { 'username': username, 'password': password }, callback);
+  $scope.onLogout = function() {
+    $scope.my_username = null;
+    socket.restart();
+  };
+
+  /**
+   * @param {string} username
+   * @param {string} password
+   * @param {string} passwordReenter
+   */
+  $scope.onSignup = function(username, password, passwordReenter) {
+    if (!username || username === '') {
+      $scope.signupWarning = 'Username is empty';
+    } else if (password === passwordReenter) {
+      signupUser(username, password);
+    } else {
+      $scope.signupWarning = 'Passwords are not the same';
     }
+    $scope.clearFields();
+  };
 
-    /**
-     * @param {string} chatMessage
-     */
-    $scope.onSend = function(chatMessage) {
-      if ($scope.selectedIndex !== -1) {
-        sendMessage(chatMessage, $scope.my_username, $scope.userList[$scope.selectedIndex]);
-      } else {
-        sendMessage(chatMessage, $scope.my_username, null);
-      }
+  $scope.clearFields = function() {
+    $scope.chatInput = '';
+    $scope.loginUsername = '';
+    $scope.loginPassword = '';
+    $scope.signupUsername = '';
+    $scope.signupPassword = '';
+    $scope.signupPasswordReenter = '';
+  };
 
-      $scope.clearFields();
-    };
+  /**
+   * @param {message} message
+   * @return {string}
+   */
+  $scope.messageColorSelect = function(message) {
+    if (message.error) {
+      return 'alert-danger';
+    } else if (message.receiver) {
+      return 'alert-info';
+    } else if (message.username) {
+      return 'alert-success';
+    } else {
+      return 'alert-warning';
+    }
+  };
 
-    /**
-     * @param {string} username
-     * @param {string} password
-     */
-    $scope.onLogin = function(username, password) {
-      if (!username || username === '') {
-        $scope.loginWarning = 'Username is empty';
-      } else if (!password || password === '') {
-        $scope.loginWarning = 'Password is empty';
-      } else {
-        loginUser(username, password);
-      }
-      $scope.clearFields();
-    };
+  /**
+   * @param {string} username
+   * @return {boolean}
+   */
+  $scope.isUserHidden = function(username) {
+    if ($scope.hiddenUsers[username]) {
+      return true;
+    } else {
+      return false;
+    }
+  };
 
-    $scope.onLogout = function() {
-      $scope.my_username = null;
-      socket.restart();
-    };
+  /**
+   * @param {string} name
+   * @return {string}
+   */
+  $scope.labelColorSelect = function(name) {
+    if (name === $scope.my_username) {
+      return 'label-success';
+    } else if (name) {
+      return 'label-primary';
+    } else {
+      return 'label-default';
+    }
+  };
 
-    /**
-     * @param {string} username
-     * @param {string} password
-     * @param {string} passwordReenter
-     */
-    $scope.onSignup = function(username, password, passwordReenter) {
-      if (!username || username === '') {
-        $scope.signupWarning = 'Username is empty';
-      } else if (password === passwordReenter) {
-        signupUser(username, password);
-      } else {
-        $scope.signupWarning = 'Passwords are not the same';
-      }
-      $scope.clearFields();
-    };
+  /**
+   * @param {string} name
+   * @return {string}
+   */
+  $scope.labelNameSelect = function(name) {
+    if (name === $scope.my_username) {
+      return 'You';
+    } else if (name) {
+      return name;
+    } else {
+      return 'Server';
+    }
+  };
 
-    $scope.clearFields = function() {
-      $scope.chatInput = '';
-      $scope.loginUsername = '';
-      $scope.loginPassword = '';
-      $scope.signupUsername = '';
-      $scope.signupPassword = '';
-      $scope.signupPasswordReenter = '';
-    };
+  /**
+   * @param {number} index
+   */
+  $scope.removeMessage = function(index) {
+    $scope.messageList.splice(index, 1);
+  };
 
-    /**
-     * @param {message} message
-     * @return {string}
-     */
-    $scope.messageColorSelect = function(message) {
-      if (message.error) {
-        return 'alert-danger';
-      } else if (message.receiver) {
-        return 'alert-info';
-      } else if (message.username) {
-        return 'alert-success';
-      } else {
-        return 'alert-warning';
-      }
-    };
-
-    /**
-     * @param {string} username
-     * @return {boolean}
-     */
-    $scope.isUserHidden = function(username) {
-      if ($scope.hiddenUsers[username]) {
-        return true;
-      } else {
-        return false;
-      }
-    };
-
-    /**
-     * @param {string} name
-     * @return {string}
-     */
-    $scope.labelColorSelect = function(name) {
-      if (name === $scope.my_username) {
-        return 'label-success';
-      } else if (name) {
-        return 'label-primary';
-      } else {
-        return 'label-default';
-      }
-    };
-
-    /**
-     * @param {string} name
-     * @return {string}
-     */
-    $scope.labelNameSelect = function(name) {
-      if (name === $scope.my_username) {
-        return 'You';
-      } else if (name) {
-        return name;
-      } else {
-        return 'Server';
-      }
-    };
-
-    /**
-     * @param {number} index
-     */
-    $scope.removeMessage = function(index) {
-      $scope.messageList.splice(index, 1);
-    };
-
-    /**
-     * @param {number} index
-     */
-    $scope.userClicked = function(index) {
-      if ($scope.selectedIndex === index) {
-        $scope.selectedIndex = -1;
-      } else {
-        $scope.selectedIndex = index;
-      }
-    };
-}]);
+  /**
+   * @param {number} index
+   */
+  $scope.userClicked = function(index) {
+    if ($scope.selectedIndex === index) {
+      $scope.selectedIndex = -1;
+    } else {
+      $scope.selectedIndex = index;
+    }
+  };
+});
---
+++ public/js/socketFactory.js
@@ -0,0 +1,29 @@
+// creates a socket that wraps the functions of socket.io
+angular.module('chatApp').factory('socket', function ($rootScope) {
+  var socket = io.connect(connectionURL);
+
+  return {
+    on: function (eventName, callback) {
+      socket.on(eventName, function () {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          callback.apply(socket, args);
+        });
+      });
+    },
+    emit: function (eventName, data, callback) {
+      socket.emit(eventName, data, function () {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          if (callback) {
+            callback.apply(socket, args);
+          }
+        });
+      });
+    }, 
+    restart: function() {
+      socket.io.disconnect();
+      socket.io.reconnect();
+    }
+  };
+});
