c19b0aeee6a202a6e82a5221a57bf3800c38b183
Darin Minamoto <darinm223@gmail.com>
2014-08-31 04:01:40 -0700

    Added some new test cases

--- test/test-server.js
+++ test/test-server.js
@@ -189,6 +189,11 @@ describe('Testing Chat Server', function () {
     });
   });
 
+  it('Test that other clients get message when client logs out', function(done) {
+    // TODO: implement this
+    return done();
+  });
+
   // should send back error
   it('Test logging in as the same user twice on different clients', function(done) {
     client1 = io.connect(socketURL, options);
@@ -207,6 +212,102 @@ describe('Testing Chat Server', function () {
       });
       client2.on('connect', function() {
         client1.emit('user:signup', { username: 'test_user', password: 'hello' });
+      });
+    });
+  });
+
+  // should send back error
+  it('Test if logged in client sends private chat message using another username', function(done) {
+    // TODO: implement this
+    return done();
+  });
+
+  it('Test if logged in client sees private chat message', function(done) {
+    client1 = io.connect(socketURL, options);
+    var messageSent = false;
+    client1.on('signup:message', function () {
+      client1.emit('user:login', { username: 'test_user', password: 'hello' });
+    });
+    client1.on('login:message', function() {
+      client2.emit('user:signup', { username: 'test_user2', password: 'hello' });
+      messageSent = true;
+    });
+    client1.on('message', function(data) {
+      if (messageSent) {
+        data.message.should.equal('test');
+        return done();
+      }
+    });
+    client1.on('connect', function() {
+      client2 = io.connect(socketURL, options);
+      client2.on('signup:message', function() {
+        client2.emit('user:login', { username: 'test_user2', password: 'hello' });
+      });
+      client2.on('login:message', function() {
+        messageSent = true;
+        client2.emit('message', { message: 'test', username: 'test_user2', receiver: 'test_user' });
+      });
+      client2.on('connect', function() {
+        client1.emit('user:signup', { username: 'test_user', password: 'hello' });
+      });
+    });
+  });
+
+  // client 3 should not see any messages and client 1 and 2 should get their 
+  // respective messages
+  it('Test if other users can see private chat between two users', function(done) {
+    client1 = io.connect(socketURL, options);
+    client1.on('signup:message', function() {
+      client1.emit('user:login', { username: 'test_user', password: 'hello' });
+    });
+    client1.on('login:message', function() {
+      client2.emit('user:signup', { username: 'test_user2', password: 'hello' });
+    });
+
+    var messageSent = false;
+    var messageSent2 = false;
+    var sawMessage = false;
+
+    client1.on('message', function(data) {
+      // if both message have been sent and client 3 hasn't seen any messages
+      if (messageSent && messageSent2 && !sawMessage) {
+        // verify client 1 gets client 2's message and exit
+        if (data.message === 'test2') {
+          return done();
+        }
+      }
+    });
+
+    client1.on('connect', function() {
+      client2 = io.connect(socketURL, options);
+      client2.on('signup:message', function() {
+        client2.emit('user:login', { username: 'test_user2', password: 'hello' });
+      });
+      client2.on('login:message', function() {
+        messageSent = true;
+        // send message from client 1 to client 2
+        client1.emit('message', { message: 'test', username: 'test_user', receiver: 'test_user2' });
+      });
+      client2.on('message', function(data) {
+        if (messageSent) {
+          // verify client 2 gets client 1's message
+          data.message.should.equal('test');
+          messageSent2 = true;
+          // send message from client 2 to client 1
+          client2.emit('message', { message: 'test2', username: 'test_user2', receiver: 'test_user' });
+        }
+      });
+
+      client2.on('connect', function() {
+        client3 = io.connect(socketURL, options);
+        client3.on('message', function(data) {
+          if (messageSent && data.message === 'test') {
+            sawMessage = true;
+          }
+        });
+        client3.on('connect', function() {
+          client1.emit('user:signup', { username: 'test_user', password: 'hello' });
+        });
       });
     });
   });
